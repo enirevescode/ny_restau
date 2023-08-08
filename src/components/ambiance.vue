71135a5bb8b44eb657e7629c805ae26178d8ba03
enireves <enirevescode@gmail.com>
2023-08-08 20:42:50 +0200

    fin mise en place card ambiance.vue

--- src/components/ambiance.vue
+++ src/components/ambiance.vue
@@ -1,48 +1,83 @@
 <template>
-
    <!-- 1ere card sans img -->
-   <v-row>
-   <v-card
-    class="d-flex justify-start h-100 pa-2 ma-5"
-    max-width="550"
-    
-  >
-    <v-card-text>
-      <p class="text-h3 font-weight-bold text--primary">   
-        Discover the Good Atmosphere of NYUS.</p><br>
-        <div class="text-h5 text-justify">Greyhound divisely hello coldly fonwderfull. Neque elit, rutrum in laoreet nec eget scelerisque volutpat sit. Bibendum tincidunt a scelerisque consectetur ultrices elementum pulvinar non. Leo ut id vel nec nisi odio nisi. Auctor egestas nibh augue enim, ornare blandit. Purus augue sagittis, risus phasellus egestas ipsum.</div>
-    </v-card-text>
+   <v-container >
+      <v-row>
+         <v-col cols="6" max-width ="520" class="mb-6">
+            <v-card class="my-10">
+               <v-card-text>
+                  <p class="text-h3 font-weight-bold text--primary mb-3">
+                     Discover the Good Atmosphere of NYUS.</p>
+                  <div class="text-body-2 text-justify">Greyhound divisely hello coldly fonwderfull. Neque elit, rutrum in
+                     laoreet nec eget scelerisque volutpat sit. Bibendum tincidunt a scelerisque consectetur ultrices
+                     elementum pulvinar non. Leo ut id vel nec nisi odio nisi. Auctor egestas nibh augue enim, ornare
+                     blandit. Purus augue sagittis, risus phasellus egestas ipsum.</div>
+               </v-card-text>
+            </v-card>
 
-    
-  </v-card>
+            <v-card class="my-10">
+               <v-card-item>
+                  <div>
+                     <v-img width ="520" height ="360" :src="require('@/assets/good-vibes.webp')" :aspect-ratio="1" cover>
 
-   <v-card
-     class="mx-auto justify-end"
-     max-width="546"
-     
-   >
-     <v-card-item>
-      <div>
-         <v-img
-      class="align-end text-white"
-      height="346"
-      src="@/assets/cozy-place.webp"
-      alt="vue restau"
-      cover
-    >
-      
-    </v-img>
-    <v-card-title>Cozy place</v-card-title>
-       <v-card-text>
-            <div class="text-h5 mb-1">
-           
-            </div>
-         </v-card-text>   
+                     </v-img>
+                     <div class="font-weight-bold text-left my-3">Good Vibes</div>
+                     <v-card-text>
+                        <div class="text-body-2 text-left font-weight-regular">
+                           In total 650m² of hand-crafted decor, mix of styles and colors.
+                        </div>
+                     </v-card-text>
+
+                  </div>
+               </v-card-item>
+            </v-card>
+
+            <v-card>
+               <v-card-item>
+                  <div>
+                     <v-img width ="520" height ="360" :src="require('@/assets/bg-image-cake.webp')" :aspect-ratio="1" cover>
+
+                     </v-img>
+                  </div>
+               </v-card-item>
+            </v-card>
+         </v-col>
+
          
-      </div>
-     </v-card-item>
- 
-     
-   </v-card>
-   </v-row>
- </template>
+
+         <v-col max-width="520" class="mb-6">
+            <v-card max-width="488" class="mb-10">
+               <v-card-item>
+                  <div>
+                     <v-img width ="520" height ="360" :src="require('@/assets/cozy-place.webp')" :aspect-ratio="1" cover>
+
+                     </v-img>
+                    
+                        <div class="font-weight-bold text-left mt-2 mb-2">Cozy Place</div>
+                     <v-card-text>
+                        <div class="text-body-2 text-left font-weight-regular">
+                           To make everyone entering the establishment feel at home and find their atmosphere.
+                        </div>
+                     </v-card-text>
+
+                  </div>
+               </v-card-item>
+            </v-card>
+            <v-card class="my-10">
+               <v-card-item>
+                  <div>
+                     <v-img width ="520" height ="360" :src="require('@/assets/relax.webp')" :aspect-ratio="1" cover>
+                     </v-img>
+                     <div class="font-weight-bold text-left my-2">Relax Atmosphere</div>
+                     <v-card-text>
+                        <div class="text-body-2 text-left font-weight-regular">
+                           Take refuge in our exclusive haven of greenery and away from the crowds. Live music and performances every Friday night!						
+                        </div>
+                     </v-card-text>
+                  </div>
+               </v-card-item>
+            </v-card>
+         </v-col>
+
+      </v-row>      
+   </v-container>
+</template>
