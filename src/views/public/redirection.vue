b45937ebd9f113c17cb22047a98e9953a50cbe11
enireves <enirevescode@gmail.com>
2023-08-14 18:09:49 +0200

    fin mise en page redirection.vue

--- src/views/public/redirection.vue
+++ src/views/public/redirection.vue
@@ -1,5 +1,6 @@
 <template>
-   <div>
+   <v-img :src="require('@/assets/architecture.jpg')">
+   <div class="text-h2 mt-16">
       oh ! La boulette, la boulette ! Cette page n'existe pas !
    </div>
    <div class="position-absolute d-flex align-center justify-center w-100 h-100">
@@ -9,7 +10,9 @@
       >
         Retour à l'accueil
       </v-btn>
+   
     </div>
+   </v-img>
 </template>
 
 <script>
