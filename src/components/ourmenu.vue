a46ba5d8e0f60336c75bd649cc95ab3180ec90bd
enireves <enirevescode@gmail.com>
2023-08-21 12:18:00 +0200

    CREATION DU MENU RESTAU

--- src/components/ourmenu.vue
+++ src/components/ourmenu.vue
@@ -1,25 +1,118 @@
 <template>
   <v-container>
+   
     <h2 class="text-h5">OUR MENU</h2>
     <br />
     <br />
     <v-row>
       <v-col cols="4">
         <v-divider :thickness="1" class="border-opacity-100 ms-12 my-5" color="white" inset
-          ><span class="divider"></span
-        ></v-divider>
+          ></v-divider>
       </v-col>
       <v-col cols="4">
         <h2 class="text-h3">Get Relax. Eat.</h2>
       </v-col>
       <v-col cols="4">
         <v-divider :thickness="1" class="border-opacity-100 ms-1 my-5" color="white" inset
-          ><span class="divider"></span
-        ></v-divider>
+          ></v-divider>
       </v-col>
     </v-row>
+<div class="text-h5 text-center font-weight-100 mt-10">Neque elit, rutrum in laoreet nec eget scelerisque volutpat sit. Bibendum tincidunt a scelerisque consectetur ultrices elementum pulvinar non.</div>
 
-    <v-table> </v-table>
+    <v-container class="mt-10">
+      <v-row
+        align="start"
+        no-gutters
+        style="height: 120px;"
+        >
+        <v-col
+          v-for="n in 2"
+          :key="n"
+        >
+          <v-sheet class="px-3 mx-3"
+          color="black"
+          >
+            <p class="text-h6 text-left">LEMON AND GARLIC GREEN BEANS - 15.00</p>
+            <p class="text-subtitle-1 text-left" color="#7a7a7a">Lemon / Garlic / Beans</p>
+            <v-divider  class="border-opacity-50 ml-1 mr-6 my-5" color="white" inset></v-divider>
+          </v-sheet>
+        </v-col>
+      </v-row>
+      <v-row
+        align="start"
+        no-gutters
+        style="height: 120px;"
+      >
+        <v-col
+          v-for="n in 2"
+          :key="n"
+        >
+          <v-sheet class="px-3 mx-3"
+          color="black"
+          >
+            <p class="text-h6 text-left">BACON-WRAPPED SHRIMP WITH GARLIC - 21.50</p>
+            <p class="text-subtitle-1 text-left" color="#7a7a7a">Bacon / Shrimp / Garlic</p>
+            <v-divider  class="border-opacity-50 ml-1 mr-6 my-5" color="white" inset></v-divider>
+          </v-sheet>
+        </v-col>
+      </v-row>
+      <v-row
+        align="start"
+        no-gutters
+        style="height: 120px;"
+      >
+        <v-col
+          v-for="n in 2"
+          :key="n"
+        >
+          <v-sheet class="px-3 mx-3"
+          color="black"
+          >
+            <p class="text-h6 text-left">LAMB-BEEF KOFKA SKEWERS WITH TZATZIKI - 18.50</p>
+            <p class="text-subtitle-1 text-left" color="#7a7a7a">Lamb / Wine / Butter</p>
+            <v-divider  class="border-opacity-50 ml-1 mr-6 my-5" color="white" inset></v-divider>
+          </v-sheet>
+        </v-col>
+      </v-row>
+      <v-row
+        align="start"
+        no-gutters
+        style="height: 120px;"
+      >
+        <v-col
+          v-for="n in 2"
+          :key="n"
+        >
+          <v-sheet class="px-3 mx-3"
+          color="black"
+          >
+            <p class="text-h6 text-left">IMPORTED OYSTER GRILL (5 Pieces) - 20.00</p>
+            <p class="text-subtitle-1 text-left" color="#7a7a7a">Oysters / Veggie / Ginger</p>
+            <v-divider  class="border-opacity-50 ml-1 mr-6 my-5" color="white" inset></v-divider>
+          </v-sheet>
+        </v-col>
+      </v-row>
+      <v-row
+        align="start"
+        no-gutters
+        style="height: 120px;"
+      >
+        <v-col
+          v-for="n in 2"
+          :key="n"
+        >
+          <v-sheet class="px-3 mx-3"
+          color="black"
+          >
+            <p class="text-h6 text-left">IMPORTED OYSTER GRILL (5 Pieces) - 20.00</p>
+            <p class="text-subtitle-1 text-left" color="#7a7a7a">Oysters / Veggie / Ginger</p>
+            <v-divider  class="border-opacity-50 ml-1 mr-6 my-5" color="white" inset></v-divider>
+          </v-sheet>
+        </v-col>
+      </v-row>
+    </v-container>
+
+ 
   </v-container>
 </template>
 <script>
@@ -28,13 +121,6 @@ export default {
 };
 </script>
 <style scoped>
-/* .divider {
-  width: 408px !important;
-  margin-top: 50px;
-  display: inline; */
-/* direction: ltr; */
-
-/* } */
 
 .v-container {
   background-color: black;
@@ -44,5 +130,14 @@ export default {
 .text-h5,
 .text-h3 {
   color: white;
+}
+
+.text-center {
+   margin-left: 260px;
+   margin-right: 260px;
+}
+
+.text-subtitle-1 {
+   color: #7a7a7a;
 }
 </style>
