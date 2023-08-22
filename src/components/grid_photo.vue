0cd928ffe3e1e090c9e7c2bce8b53ce6e4732717
enireves <enirevescode@gmail.com>
2023-08-22 21:11:27 +0200

    créa de la grid avec boucle

---
+++ src/components/grid_photo.vue
@@ -0,0 +1,39 @@
+<template>
+   <!-- 1ere card sans img -->
+   <v-container>
+      <v-row class="mt-6">
+         <v-col
+         v-for="photo in photos"
+         cols="4" 
+         max-width ="410" 
+         height="274" 
+         class="mb-6">
+            
+
+                     <v-img 
+                     :src="photo.src" 
+                     cover hide-delimiter>
+
+                     </v-img>
+                 
+         </v-col>
+      </v-row>      
+   </v-container>
+</template>
+<script>
+export default {
+   name: "grid_photo",
+   data: () => ({
+      photos: [
+        { src: "@/assets/fine-dining-food.webp" },
+        { title: 'Favorite road trips', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 6 },
+        { title: 'Best airlines', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 6 },
+      ],
+    }),
+}
+</script>
+<style>
+.v-container{
+   max-width: 100%;
+}
+</style>
