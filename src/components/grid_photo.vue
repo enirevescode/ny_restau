b93bbeca93881594a3fddc19c54cdc0fc657ed87
enireves <enirevescode@gmail.com>
2023-08-26 20:30:18 +0200

    correction aff. photo

--- src/components/grid_photo.vue
+++ src/components/grid_photo.vue
@@ -4,6 +4,8 @@
       <v-row class="mt-6">
          <v-col
          v-for="photo in photos"
+         :key="photo.src"
+         alt="preparation plat"
          cols="4" 
          max-width ="410" 
          height="274" 
@@ -24,13 +26,16 @@
 export default {
    name: "grid_photo",
    data: () => ({
+    
       photos: [
-        { src: "@/assets/fine-dining-food.webp" },
-        { title: 'Favorite road trips', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 6 },
-        { title: 'Best airlines', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 6 },
+        { src: "require('@/assets/fine-dining-food.webp')", flex: 4 },
+        { src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 4 },
+        { title: 'Best airlines', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 4 },
       ],
-    }),
+   }),
+    
 }
+
 </script>
 <style>
 .v-container{
