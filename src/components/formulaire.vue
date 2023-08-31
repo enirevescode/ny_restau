3303813248affce48f3508fc894c17b5e057081d
enireves <enirevescode@gmail.com>
2023-08-31 20:04:07 +0200

    fin formulaire

--- src/components/formulaire.vue
+++ src/components/formulaire.vue
@@ -1,12 +1,20 @@
  <template>
-  <v-sheet width="300" class="mx-auto">
-   <div class="text-h4">Send Us a Message</div>
-   <div class="text-h5 text-caption">Massa praesent sit suspendisse ac volutpat amet. Commodo elit at non neque ullamcorper id.</div>
+  <v-sheet class="formulaire">
+   
    <v-row>
    <v-col
-   :cols="6"><iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d21764.740399184175!2d-2.2511616!3d47.00897279999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sfr!2sfr!4v1693502779991!5m2!1sfr!2sfr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></v-col>
+   cols="6"
+   class="d-flex flex-column"
+   max-width="300"
+   order="first"><iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d21764.740399184175!2d-2.2511616!3d47.00897279999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sfr!2sfr!4v1693502779991!5m2!1sfr!2sfr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></v-col>
+
    <v-col
-   :cols="6">
+   cols="5"
+   class="d-flex flex-column"
+   max-width="200"
+   >
+   <div class="text-h4">Send Us a Message</div>
+   <div class="text-h5 text-caption">Massa praesent sit suspendisse ac volutpat amet. Commodo elit at non neque ullamcorper id.</div>
     <v-form @submit.prevent>
       <v-text-field
         v-model="firstName"
@@ -45,3 +53,10 @@
     }),
   }
 </script>
+<style>
+.formulaire {
+   margin-top: 100px;
+   margin-left: 50px;
+   margin-right: 50px;
+}
+</style>
