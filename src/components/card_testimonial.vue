f4fbaaec9cf6e7fc08fbec3693a81f15de9fe8b2
enirevescode <enirevescode@gmail.com>
2023-09-15 14:51:50 +0200

    fin refacto card

--- src/components/card_testimonial.vue
+++ src/components/card_testimonial.vue
@@ -1,30 +1,26 @@
 <template>
   <div class="testi">
-  <h3 class="text-h5">TESTIMONIAL</h3>
-  <h2 class="text-h2">What They Say</h2>
+  <h3 class="titre1">TESTIMONIAL</h3>
+  <h2 class="titre2">What They Say</h2>
   
     
  <v-container>
    <v-row no-gutters>
      <v-col v-for="temoignage in temoignages" :key="temoignage.id">
-       <v-avatar size="95" color="#f5c61c">
-        <v-img width ="80" height ="80" :src="require('@/assets/kyle.png')" :aspect-ratio="1" class= "mx-auto" >
-       </v-img>
+       <v-avatar size="95" color="#f5c61c" class="mx-auto">
+        <img width ="80" height ="80" :src="temoignage.img" :aspect-ratio="1" class="mx-auto"/>
        </v-avatar>
        <v-sheet 
-       class="pt-16 px-6 mb-n16 bg-black text-h6"
+       class="pt-16 px-6 mb-n16 bg-black"
        width="367px"
-       height="319px"
+       height="320px"
        rounded="lg"
        >
        <div class="text">
-        <p>{{ temoignage.avis }}</p>
+        <p class="avis">{{ temoignage.avis }}</p>
         <p class="name">{{ temoignage.name }}</p>
        </div>
-         
-       
 
-        
 
        </v-sheet>
      </v-col>
@@ -37,24 +33,24 @@
 const temoignages = [
          {
        "id": 1,
-       "img": "src/assets/emma.webp",
+       "img": "../assets/emma.webp",
        "alt": "portrait",
        "name": "Emma Odinson",
-       "avis": "Rhoncus risus donec neque egestas ultrices ullamcorper cursus aliquam nunc. Cras est sit libero, est in scelerisque donec. Tincidunt malesuada odio neque et vel fringilla elit. Placerat turpis mus donec id vel"
+       "avis": "Risus donec neque egestas ultrices ullamcorper cursus aliquam nunc. Cras est sit libero, est in scelerisque donec. Tincidunt malesuada odio neque et vel fringilla elit. Placerat turpis mus donec id vel"
           },
           {
        "id": 2,
        "img": "src/assets/diana.webp",
        "alt": "portrait",
        "name":"Diana Annakin",
-       "avis": "Rhoncus risus donec neque egestas ultrices ullamcorper cursus aliquam nunc. Cras est sit libero, est in scelerisque donec. Tincidunt malesuada odio neque et vel fringilla elit. Placerat turpis mus donec id vel"
+       "avis": "Donec neque egestas ultrices ullamcorper cursus aliquam nunc. Cras est sit libero, est in scelerisque donec. Tincidunt malesuada odio neque et vel fringilla elit. Placerat turpis mus donec id vel"
           },
           {
        "id": 3,
        "img": "src/assets/kyle.webp",
        "alt": "portrait",
        "name":"Kyle Smith",
-       "avis": "Rhoncus risus donec neque egestas ultrices ullamcorper cursus aliquam nunc. Cras est sit libero, est in scelerisque donec. Tincidunt malesuada odio neque et vel fringilla elit. Placerat turpis mus donec id vel"
+       "avis": "Neque egestas ultrices ullamcorper cursus aliquam nunc. Cras est sit libero, est in scelerisque donec. Tincidunt malesuada odio neque et vel fringilla elit. Placerat turpis mus donec id vel"
           },
 
 ]
@@ -68,31 +64,42 @@ const temoignages = [
   height: 669px;
 }
 
-
-.text-h5{
+.titre1{
   color: white;
   font-weight: 700;
   padding-bottom: 50px;
   padding-top: 70px;
   letter-spacing: 2px;
   line-height: 1.7;
+  text-align: center;
 }
 
-.text-h2{
+.titre2{
   line-height: 1.3;
   color: white;
   font-size: 3rem;
   font-weight: 700;
   letter-spacing: 2px;
+  text-align: center;
 }
 
 .v-avatar {
  z-index: 1000;
  margin-bottom: -40px;
+ display: block;
 }
 
 .v-container {
  margin-top: 120px;
 }
-
+.name {
+  text-align: center;
+  margin-bottom: 5px;
+  margin-top: 5px;
+}
+.avis {
+  text-align: justify;
+  font-size: 18px;
+  font-weight: 400;
+}
 </style>
