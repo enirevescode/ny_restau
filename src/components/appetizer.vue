a1aad1c3ceb33cf7fb1eb8fb460ad6c654cfecbf
enirevescode <enirevescode@gmail.com>
2023-09-25 19:58:36 +0200

    supression info sur appetizer

--- src/components/appetizer.vue
+++ src/components/appetizer.vue
@@ -28,44 +28,6 @@
     </v-col>
 </v-col>
 </v-row>
-
-
-    <v-sheet
-    class="d-flex align-center justify-center flex-wrap text-center mx-auto px-16"
-    color="transparent"
-    elevation="0"
-    height="550"
-    rounded
-    max-width="900"
-    width="100%"
-  >
-  
-    <div>
-      <h1 class="text-h4 font-weight-black mt-16">Réservation</h1>
-
-      <div class="text-h6 font-weight-medium my-2">
-        Neque elit, rutrum in laoreet nec eget scelerisque volutpat sit. Bibendum tincidunt a scelerisque consectetur ultrices elementum pulvinar non.
-      </div>
-
-      <v-btn variant="outlined"
-      class="text-h5"
-      >Book A Table</v-btn>
-      <v-spacer></v-spacer>
-
-      <p class="text-h3 my-4">
-        
-       <v-icon icon="mdiPhone"></v-icon>Telephone Reservations
-      </p>
-<p class="tel">12 34 56 7829</p>
-      </div>
-
-      <v-img
-      :src="require('@/assets/cake.webp')"
-      width="275"
-      height="400"
-      id="cake"
-      ></v-img>
-  </v-sheet>
 </template>
 <script>
 
